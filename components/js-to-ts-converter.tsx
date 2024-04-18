880d67150721730b17239492896700d0f8516a25
Shane O'Sullivan <shaneosullivan1@gmail.com>
2024-04-18 18:44:42 +0100

    Improve layout of the site on mobile devices. Arrange textareas vertically instead of horizontally

--- components/js-to-ts-converter.tsx
+++ components/js-to-ts-converter.tsx
@@ -30,7 +30,7 @@ export function JsToTsConverter() {
       const tsValue = convertToTs(value);
       setResultValue(tsValue);
     } catch (err) {
-      window.alert("Error: Invalid JSON");
+      toast.error("Error: Invalid JSON", {});
     }
   }
 
@@ -43,7 +43,7 @@ export function JsToTsConverter() {
     <div className="flex flex-col h-screen bg-gradient-to-r from-[#FF6B6B] to-[#FFD166] p-4">
       <header className="flex items-center">
         <div className="p-0">
-          <h1 className="text-2xl font-semibold text-white">
+          <h1 className="text-xl sm:text-2xl font-semibold text-white">
             JSON to TypeScript Converter
           </h1>
         </div>
@@ -53,7 +53,7 @@ export function JsToTsConverter() {
           <a
             href="https://chofter.com"
             target="_blank"
-            className="inline-flex items-center justify-center rounded-full bg-gradient-to-r from-[#6366f1] to-[#9333ea] px-4 py-2 text-sm font-medium text-white shadow-lg transition-colors hover:from-[#4f46e5] hover:to-[#7e22ce] focus:outline-none focus:ring-2 focus:ring-[#6366f1] focus:ring-offset-2 dark:focus:ring-offset-gray-800"
+            className="hidden sm:inline-flex inline-flex items-center justify-center rounded-full bg-gradient-to-r from-[#6366f1] to-[#9333ea] px-4 py-2 text-sm font-medium text-white shadow-lg transition-colors hover:from-[#4f46e5] hover:to-[#7e22ce] focus:outline-none focus:ring-2 focus:ring-[#6366f1] focus:ring-offset-2 dark:focus:ring-offset-gray-800"
           >
             Built by me
             <ExternalLinkIcon className="ml-2 h-4 w-4" />
@@ -65,7 +65,7 @@ export function JsToTsConverter() {
             <img src="/github-mark.svg" style={{ height: "36px" }} />
           </a>
         </div>
-        <div className="flex flex-row w-full h-full gap-2">
+        <div className="flex flex-col sm:flex-row w-full h-full gap-2">
           <div className="flex flex-col flex-1">
             <Textarea
               className="flex-1 bg-white text-gray-900 dark:bg-gray-800 dark:text-gray-300"
@@ -74,8 +74,8 @@ export function JsToTsConverter() {
               value={sourceValue}
             />
           </div>
-          <div className="flex items-center">
-            <div className="flex flex-col gap-2">
+          <div className="flex justify-center sm:items-center">
+            <div className="flex flex-row sm:flex-col gap-2">
               <Button
                 className="bg-yellow-400 text-zinc-500 hover:bg-yellow-500 dark:bg-yellow-600 dark:text-gray-900 dark:hover:bg-yellow-700"
                 onClick={doConversion}
